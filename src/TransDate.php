25393454534e33c0720b1faea2b0bb90db0a085e
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-12-26 08:01:27 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/TransDate.php
+++ src/TransDate.php
@@ -18,222 +18,222 @@
             //$period is number, $dateArray[1] is the time $dateArray[0] , $dateArray[2] is the word ago
             $time = $dateArray[1];
             $period = $dateArray[0];
-            if((new self)->isSecond($time))
+            if ((new self)->isSecond($time))
             {
-                return (new self)->handelSeconds($period,$zaman);
+                return (new self)->handelSeconds($period, $zaman);
             }
             // handl minutes
-            if((new self)->isMinute($time))
+            if ((new self)->isMinute($time))
             {
-                return (new self)->handelMinutes($period,$zaman);
+                return (new self)->handelMinutes($period, $zaman);
             }
             // handl hours
-            if((new self)->isHour($time))
+            if ((new self)->isHour($time))
             {
-                return (new self)->handelHours($period,$zaman);
+                return (new self)->handelHours($period, $zaman);
             }
             // handl days
-            if((new self)->isDay($time))
+            if ((new self)->isDay($time))
             {
-                return (new self)->handelDays($period,$zaman);
+                return (new self)->handelDays($period, $zaman);
             }
             // handl weeks
-            if((new self)->isWeek($time))
+            if ((new self)->isWeek($time))
             {
-                return (new self)->handelWeeks($period,$zaman);
+                return (new self)->handelWeeks($period, $zaman);
             }
             // handl months
-            if((new self)->isMonth($time))
+            if ((new self)->isMonth($time))
             {
-                return (new self)->handelMonths($period,$zaman);
+                return (new self)->handelMonths($period, $zaman);
             }
             // handl years
-            if((new self)->isYear($time))
+            if ((new self)->isYear($time))
             {
-                return (new self)->handelYears($period,$zaman);
+                return (new self)->handelYears($period, $zaman);
             }
         }
 
         public function isSecond($time)
         {
-            Str::startsWith($time,'second') ? $isSecond = true : $isSecond = false ;
+            Str::startsWith($time, 'second') ? $isSecond = true : $isSecond = false;
             return $isSecond;
         }
 
         public function isMinute($time)
         {
-            Str::startsWith($time,'minute') ? $isMinute = true : $isMinute = false ;
+            Str::startsWith($time, 'minute') ? $isMinute = true : $isMinute = false;
             return $isMinute;
         }
 
         public function isHour($time)
         {
-            Str::startsWith($time,'hour') ? $isHour = true : $isHour = false ;
+            Str::startsWith($time, 'hour') ? $isHour = true : $isHour = false;
             return $isHour;
         }
 
         public function isDay($time)
         {
-            Str::startsWith($time,'day') ? $isDay = true : $isDay = false ;
+            Str::startsWith($time, 'day') ? $isDay = true : $isDay = false;
             return $isDay;
         }
 
         public function isWeek($time)
         {
-            Str::startsWith($time,'week') ? $isWeek = true : $isWeek = false ;
+            Str::startsWith($time, 'week') ? $isWeek = true : $isWeek = false;
             return $isWeek;
         }
 
         public function isMonth($time)
         {
-            Str::startsWith($time,'month') ? $isMonth = true : $isMonth = false ;
+            Str::startsWith($time, 'month') ? $isMonth = true : $isMonth = false;
             return $isMonth;
         }
 
         public function isYear($time)
         {
-            Str::startsWith($time,'year') ? $isYear = true : $isYear = false ;
+            Str::startsWith($time, 'year') ? $isYear = true : $isYear = false;
             return $isYear;
         }
 
-        public function handelSeconds($period,$zaman)
+        public function handelSeconds($period, $zaman)
         {
             // handl seconds
-            if($period == 1)
+            if ($period == 1)
             {
                 return $zaman."ثانية واحدة";
             }
-            elseif($period == 2)
+            elseif ($period == 2)
             {
-                return $zaman. "ثانيتين";
+                return $zaman."ثانيتين";
             }
-            elseif($period >= 3 && $period <= 10  )
+            elseif ($period >= 3 && $period <= 10)
             {
-                return $zaman. $period . " ثواني";
+                return $zaman.$period." ثواني";
             }
             else
             {
-                return $zaman.  $period . " ثانية";
+                return $zaman.$period." ثانية";
             }
         }
 
-        public function handelMinutes($period,$zaman)
+        public function handelMinutes($period, $zaman)
         {
-            if($period == 1)
+            if ($period == 1)
             {
                 return $zaman."دقيقة واحدة";
             }
-            elseif($period == 2)
+            elseif ($period == 2)
             {
-                return $zaman. "دقيقتين";
+                return $zaman."دقيقتين";
             }
-            elseif($period >= 3 && $period <= 10  )
+            elseif ($period >= 3 && $period <= 10)
             {
-                return $zaman. $period . " دقائق";
+                return $zaman.$period." دقائق";
             }
             else
             {
-                return $zaman.  $period . " دقيقة";
+                return $zaman.$period." دقيقة";
             }
         }
 
-        public function handelHours($period,$zaman)
+        public function handelHours($period, $zaman)
         {
-            if($period == 1)
+            if ($period == 1)
             {
                 return $zaman."ساعة";
             }
-            elseif($period == 2)
+            elseif ($period == 2)
             {
-                return $zaman. "ساعتين";
+                return $zaman."ساعتين";
             }
-            elseif($period >= 3 && $period <= 10  )
+            elseif ($period >= 3 && $period <= 10)
             {
-                return $zaman. $period . " ساعات";
+                return $zaman.$period." ساعات";
             }
             else
             {
-                return $zaman.  $period . " ساعة";
+                return $zaman.$period." ساعة";
             }
         }
 
-        public function handelDays($period,$zaman)
+        public function handelDays($period, $zaman)
         {
-            if($period == 1)
+            if ($period == 1)
             {
                 return $zaman." يوم";
             }
-            elseif($period == 2)
+            elseif ($period == 2)
             {
-                return $zaman. "يومين";
+                return $zaman."يومين";
             }
-            elseif($period >= 3 && $period <= 10  )
+            elseif ($period >= 3 && $period <= 10)
             {
-                return $zaman. $period . " أيام";
+                return $zaman.$period." أيام";
             }
             else
             {
-                return $zaman.  $period . " يوم";
+                return $zaman.$period." يوم";
             }
         }
 
-        public function handelWeeks($period,$zaman)
+        public function handelWeeks($period, $zaman)
         {
-            if($period == 1)
+            if ($period == 1)
             {
                 return $zaman." أسبوع";
             }
-            elseif($period == 2)
+            elseif ($period == 2)
             {
-                return $zaman. " أسبوعين";
+                return $zaman." أسبوعين";
             }
-            elseif($period >= 3 && $period <= 10  )
+            elseif ($period >= 3 && $period <= 10)
             {
-                return $zaman. $period . " أسابيع";
+                return $zaman.$period." أسابيع";
             }
             else
             {
-                return $zaman.  $period . " أسبوع";
+                return $zaman.$period." أسبوع";
             }
         }
 
-        public function handelMonths($period,$zaman)
+        public function handelMonths($period, $zaman)
         {
-            if($period == 1)
+            if ($period == 1)
             {
                 return $zaman."شهر";
             }
-            elseif($period == 2)
+            elseif ($period == 2)
             {
-                return $zaman. " شهرين";
+                return $zaman." شهرين";
             }
-            elseif($period >= 3 && $period <= 10  )
+            elseif ($period >= 3 && $period <= 10)
             {
-                return $zaman. $period . " شهور";
+                return $zaman.$period." شهور";
             }
             else
             {
-                return $zaman.  $period . " شهر";
+                return $zaman.$period." شهر";
             }
         }
 
-        public function handelYears($period,$zaman)
+        public function handelYears($period, $zaman)
         {
-            if($period == 1)
+            if ($period == 1)
             {
                 return $zaman."سنة";
             }
-            elseif($period == 2)
+            elseif ($period == 2)
             {
-                return $zaman. " سنتين";
+                return $zaman." سنتين";
             }
-            elseif($period >= 3 && $period <= 10  )
+            elseif ($period >= 3 && $period <= 10)
             {
-                return $zaman. $period . " سنوات";
+                return $zaman.$period." سنوات";
             }
             else
             {
-                return $zaman.  $period . " سنة";
+                return $zaman.$period." سنة";
             }
         }
 
